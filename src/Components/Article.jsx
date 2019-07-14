cce5b4180c29513a66879ab1aa6b01aeddcb6339
Mimi <mi.amsalem@gmail.com>
2019-07-15 01:34:57 +0200

    finished article page

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -20,16 +20,18 @@ const Article = () => {
       </div>
       <div className="article-text">
         <div className="text"> 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor nisi, ullamcorper et dapibus vitae, cursus a tellus. Proin egestas, nunc sit amet consequat imperdiet, metus ante accumsan est, non faucibus ex justo vitae leo. Fusce quis ex eleifend, dapibus dolor at, posuere mauris. Duis pellentesque aliquet ex, faucibus luctus nulla sagittis a. Quisque iaculis neque sed volutpat dignissim. Etiam fringilla orci lorem, ac aliquam mauris suscipit sed. <br/>
-          <p className="highlight">Provident atque, aperiam assumenda tempora est hic! Saepe, harum.</p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor nisi, ullamcorper et dapibus vitae, cursus a tellus. Proin egestas, nunc sit amet consequat imperdiet, metus ante accumsan est, non faucibus ex justo vitae leo. Fusce quis ex eleifend, dapibus dolor at, posuere mauris. Duis pellentesque aliquet ex, faucibus luctus nulla sagittis a. Quisque iaculis neque sed volutpat dignissim. Etiam fringilla orci lorem, ac aliquam mauris suscipit sed.
+          
+           <br/>
+          <p className="highlight">“Provident atque, aperiam assumenda tempora est hic! Saepe, harum.Etiam lacinia, dui eleifend vehicula pulvinar”</p>
 
-        <br/>
           Morbi consequat eu quam in tempus. Etiam auctor, magna sed tempus venenatis, arcu ante malesuada lorem, a hendrerit ex massa eu justo. Fusce varius malesuada diam eget sagittis. Phasellus vehicula justo nec sem iaculis, id dignissim urna placerat.
           <br/>
-          <p className="highlight">Etiam lacinia, dui eleifend vehicula pulvinar, mi lorem ultrices neque.</p>
-        <br/>
+       
           Cras quis bibendum tortor. Praesent ullamcorper iaculis nulla, quis laoreet nulla. Etiam lacinia, dui eleifend vehicula pulvinar, mi lorem ultrices neque, eu suscipit diam nibh maximus magna. Cras metus augue, cursus vel ex id, blandit pretium justo. In hac habitasse platea dictumst.
+           {/* <p className="highlight"> “Etiam lacinia, dui eleifend vehicula pulvinar, mi lorem ultrices neque. Etiam lacinia, dui eleifend vehicula pulvinar”</p> */}
         </div>
+
       </div>
   
     
